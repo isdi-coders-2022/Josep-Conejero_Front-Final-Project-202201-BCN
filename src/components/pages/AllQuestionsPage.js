93722113d79b14770942915d8cf411fe1dd1a0de
Josep Conejero <bermeu@gmail.com>
2022-03-12 23:20:11 +0100

    Added global vars to ./src/components/pages/AllQuestionsPage.js

--- src/components/pages/AllQuestionsPage.js
+++ src/components/pages/AllQuestionsPage.js
@@ -1,15 +1,21 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import MainBody from "../MainBody/MainBody";
+import {
+  textIconAllQuestions,
+  iconAllQuestionsPath,
+  backgroundColorIconAllQuestions,
+  alternativeTextIconAllQuestions,
+} from "../../variables";
 
 const AllQuestionsPage = () => {
   return (
     <>
       <Header
-        title="ALL QUESTIONS"
-        picture="img/all-questions.svg"
-        backgroundColor="#FFB100"
-        alternativeTextImage="text"
+        title={textIconAllQuestions}
+        picture={iconAllQuestionsPath}
+        backgroundColor={backgroundColorIconAllQuestions}
+        alternativeTextImage={alternativeTextIconAllQuestions}
       ></Header>
       <MainBody></MainBody>
       <Footer></Footer>
