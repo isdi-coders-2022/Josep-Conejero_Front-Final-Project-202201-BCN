ef12b3fe2630b7a3cbc25720c762665bf0cced0c
Josep Conejero <bermeu@gmail.com>
2022-03-12 23:12:59 +0100

    Added file variables.js

---
+++ src/variables.js
@@ -0,0 +1,47 @@
+export const applicationName = "LEARNING HELPER";
+export const iconApplicationPath = "img/laptopandbooks.svg";
+
+export const iconListsPath = "img/lists.svg";
+export const iconAllQuestionsPath = "img/all-questions.svg";
+export const iconAQuestionPath = "img/a-question.svg";
+export const iconReviewPath = "img/review 2.svg";
+
+export const iconBackPath = "img/back.png";
+export const iconHomePath = "img/home.png";
+export const iconAddPath = "img/add.png";
+
+export const backgroundColorIconLists = "#00E6FF";
+export const backgroundColorIconAllQuestions = "#FFB100";
+export const backgroundColorIconAQuestion = "#FFFE00";
+export const backgroundColorIconReview = "#28FF00";
+
+export const alternativeTextApplicationIcon =
+  "Application Icon. Screen and books.";
+
+export const alternativeTextIconLists = "Several lists";
+export const alternativeTextIconAllQuestions = "A shelving with books";
+export const alternativeTextIconAQuestion = "Two screens and a pencil";
+export const alternativeTextIconReview = "A notebook with a pencil";
+
+export const textIconLists = "LISTS";
+export const textIconAllQuestions = "ALL QUESTIONS";
+export const textIconAQuestion = "CREATE A QUESTION";
+export const textIconReview = "REVIEW";
+
+export const textIconQuestionsList = "QUESTIONS LIST";
+export const textIconCreateAList = "CREATE A LIST";
+export const textIconCreateRemoveFromList = "CREATE/REMOVE FROM A LIST";
+
+export const mainTextColor = "#d715c3";
+export const applicationNameTextColor = "#453";
+
+export const titleTextButtonColor = "#fff";
+export const selectedTitleTextButtonColor = "#f4ff00";
+
+export const buttonColor = "#d715c3";
+export const selectedButtonColor = "#2000ff";
+
+export const mainBackgroundColor = "#fff";
+export const headerBackgroundColor = "#e5e5e5";
+export const footerBackgroundColor = "#e5e5e5";
+export const homeBackgroundColor = "#e5e5e5";
