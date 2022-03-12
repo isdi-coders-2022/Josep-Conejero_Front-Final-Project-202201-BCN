6992f4d1e674d83d6d1febd7dff077991a861e9f
Josep Conejero <bermeu@gmail.com>
2022-03-12 12:32:48 +0100

    Added ./src/components/MainBody/MainBody.js

---
+++ src/components/MainBody/MainBody.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+  width: 100%;
+  background-color: #ddd;
+  min-height: 80vh;
+  border: 0px;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+`;
+
+const ImageBox = styled.div`
+  border-radius: 10px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+`;
+
+const Image = styled.img`
+  border: solid black 1px;
+  background-color: ${(props) => props.inputColor};
+  border-radius: 10px;
+  height: 40px;
+  width: 40px;
+  padding: 5px;
+`;
+
+const TitleText = styled.p`
+  color: black;
+  padding: 0;
+  background-color: transparent;
+  text-align: center;
+  font-size: 22px;
+  margin: 10px;
+`;
+
+const MainBody = () => {
+  return (
+    <>
+      <Box>
+        {/*         <ImageBox>
+          <Image
+            inputColor={backgroundColor}
+            alt={alternativeTextImage}
+            src={picture}
+          ></Image>
+        </ImageBox>
+ */}
+      </Box>
+    </>
+  );
+};
+
+export default MainBody;
