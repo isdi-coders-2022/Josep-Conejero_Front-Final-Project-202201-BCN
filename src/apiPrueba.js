06586b2f5a614a116b25c782a3a402b7d4dc5fb3
Josep Conejero <bermeu@gmail.com>
2022-03-13 11:08:17 +0100

    Added apiPrueba.js with an array inside

---
+++ src/apiPrueba.js
@@ -0,0 +1,19 @@
+const apiPrueba = [
+  {
+    id: 1,
+    question: "Pregunta 1",
+    answer: "Respuesta 1",
+  },
+  {
+    id: 2,
+    question: "Pregunta 2",
+    answer: "Respuesta 2",
+  },
+  {
+    id: 3,
+    question: "Pregunta 3",
+    answer: "Respuesta 3",
+  },
+];
+
+export default apiPrueba;
