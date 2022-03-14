6af6c8dc5780c01b4d84554d95dbdd0f248d0f55
Josep Conejero <bermeu@gmail.com>
2022-03-14 19:44:14 +0100

    Added ./src/components/pages/HomePage.test.js

---
+++ src/components/pages/HomePage.test.js
@@ -0,0 +1,31 @@
+/* import { render, screen } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import HomePage from "./HomePage"; */
+
+describe("Given a component HomePage", () => {
+  describe("When it's rendered", () => {
+    test("it should render an article", () => {
+      /* render(
+        <Router>
+          <HomePage></HomePage>
+        </Router>
+      );
+
+      const logo = screen.getByRole("img");
+
+      expect(logo).toBeInTheDocument(); */
+    });
+
+    test("it should render an image", () => {
+      /*  render(
+        <Router>
+          <HomePage></HomePage>
+        </Router>
+      );
+
+      const title = screen.getByRole("h1");
+
+      expect(title).toBeInTheDocument(); */
+    });
+  });
+});
