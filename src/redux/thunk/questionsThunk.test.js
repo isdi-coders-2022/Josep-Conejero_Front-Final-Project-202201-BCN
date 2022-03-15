2baa1cfab755edb83735f03c578fbb938de399af
Josep Conejero <bermeu@gmail.com>
2022-03-15 10:40:23 +0100

    Added ./src/redux/thunk/questionsThunk.test.js

--- src/redux/thunk/questionsThunk.test.js
+++ src/redux/thunk/questionsThunk.test.js
@@ -1,13 +1,13 @@
-/* import { loadQuestionsListThunk } from "./questionsThunk"; */
+import { loadQuestionsListThunk } from "./questionsThunk";
 
 describe("Given a loadQuestionsListThunk function", () => {
   describe("When it is called", () => {
     test("Then it should call the dispatch function", async () => {
-      /*     const dispatch = jest.fn();
+      const dispatch = jest.fn();
 
       await loadQuestionsListThunk(dispatch);
 
-      expect(dispatch).toHaveBeenCalled(); */
+      expect(dispatch).toHaveBeenCalled();
     });
   });
 });
