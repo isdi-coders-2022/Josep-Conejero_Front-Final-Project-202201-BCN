9d13e4c8967974d77229de2780a85dbe75752ff0
Josep Conejero <bermeu@gmail.com>
2022-03-15 14:04:01 +0100

    Added delete, add and update to actionsCreator.js

--- src/redux/actions/actionsCreator/actionsCreator.js
+++ src/redux/actions/actionsCreator/actionsCreator.js
@@ -4,3 +4,18 @@ export const loadQuestionsAction = (questions) => ({
   type: actionTypes.loadQuestions,
   questions,
 });
+
+export const deleteQuestionAction = (idQuestion) => ({
+  type: actionTypes.deleteQuestion,
+  idQuestion,
+});
+
+export const addQuestionAction = (question) => ({
+  type: actionTypes.addQuestion,
+  question,
+});
+
+export const updateQuestionAction = (idQuestion) => ({
+  type: actionTypes.updateQuestion,
+  idQuestion,
+});
