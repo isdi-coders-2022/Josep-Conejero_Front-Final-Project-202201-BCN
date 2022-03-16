534b391ed5f2cbf91c7eb918d55d278d0739dda3
Josep Conejero <bermeu@gmail.com>
2022-03-16 22:21:14 +0100

    Added create and design to MainQuestionBody

--- src/components/MainQuestionBody/MainQuestionBody.js
+++ src/components/MainQuestionBody/MainQuestionBody.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import {
   mainBackgroundColor,
-  iconDeletePath,
+  /* iconDeletePath, */
   footerBackgroundColor,
-  /* iconLittleListsPath, */
-  iconLittleQuestionPath,
+  buttonColor,
 } from "../../variables";
-import ListedQuestion from "../ListedQuestion/ListedQuestion";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { addQuestionThunk } from "../../redux/thunk/questionsThunk";
 
 const Container = styled.div`
   width: 100%;
@@ -20,27 +21,181 @@ const Container = styled.div`
 `;
 
 const QuestionBox = styled.div`
-wi
+  width: 90%;
+  height: 15vh;
+  border-radius: 10px;
+  padding: 5px;
+  border: solid black 1px;
+  margin-top: 25px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
+const AnswerBox = styled.div`
+  width: 90%;
+  height: 45vh;
+  border-radius: 10px;
+  padding: 5px;
+  border: solid black 1px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const VoidBox = styled.div`
   width: 100%;
   height: 80px;
-  /* background-color: transparent; */
   background-color: ${footerBackgroundColor};
 `;
 
-const MainQuestionBody = ({ idQuestion }) => {
+const Buttons = styled.div`
+  /* background-color: red; */
+  width: 100%;
+  height: 10vh;
+  position: fixed;
+  bottom: 10vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const AddRemoveButton = styled.button`
+  background-color: ${buttonColor};
+  border: 0px;
+  height: 6vh;
+  font-size: 20px;
+  color: white;
+  border-radius: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+  transition: 0.25s;
+  &:hover {
+    height: 7vh;
+    padding-left: 10px;
+    padding-right: 10px;
+    font-size: 22px;
+    box-shadow: 10px 10px 10px grey;
+    margin-left: 0px;
+    margin-right: 0px;
+  }
+`;
+
+const SaveButton = styled.button`
+  background-color: ${buttonColor};
+  border: 0px;
+  height: 6vh;
+  font-size: 20px;
+  color: white;
+  border-radius: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-left: 7px;
+  margin-right: 7px;
+  transition: 0.25s;
+  &:hover {
+    height: 7vh;
+    padding-left: 10px;
+    padding-right: 10px;
+    font-size: 25px;
+    box-shadow: 10px 10px 10px grey;
+    margin-left: 0px;
+    margin-right: 0px;
+  }
+`;
+
+const QuestionInput = styled.textarea`
+  /* background-color: green; */
+  width: 100%;
+  height: 20vh;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  resize: none;
+  position: relative;
+  top: -17px;
+`;
+
+const AnswerInput = styled.textarea`
+  /* background-color: green; */
+  width: 100%;
+  height: 50vh;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  resize: none;
+  position: relative;
+  top: -17px;
+`;
+
+const LabelText = styled.p`
+  margin: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+  background-color: white;
+  padding-left: 10px;
+  padding-right: 10px;
+  border: solid black 1px;
+  border-radius: 10px;
+  position: relative;
+  top: -17px;
+`;
+
+const MainQuestionBody = ({ questionText, answerText, idQuestion }) => {
+  const dispatch = useDispatch();
+  const initialFormData = {
+    question: questionText ? questionText : "",
+    answer: answerText ? answerText : "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const isFilled = formData.questionText !== "" && formData.answerText !== "";
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value.trim(),
+    });
+  };
+
+  const addQuestionHandler = (event) => {
+    event.preventDefault();
+    if (isFilled) {
+      dispatch(addQuestionThunk(formData));
+    }
+  };
+
   return (
     <>
       <VoidBox></VoidBox>
       <Container>
-        <QuestionBox></QuestionBox>
-        <input></input>
-        <input></input>
+        <QuestionBox>
+          <LabelText>QUESTION</LabelText>
+          <QuestionInput
+            onChange={handleChange}
+            name="question"
+            value={questionText}
+          ></QuestionInput>
+        </QuestionBox>
+        <AnswerBox>
+          <LabelText>ANSWER</LabelText>
+          <AnswerInput
+            onChange={handleChange}
+            name="answer"
+            value={answerText}
+          ></AnswerInput>
+        </AnswerBox>
+        <Buttons>
+          <AddRemoveButton>ADD/REMOVE FROM LIST</AddRemoveButton>
+          <SaveButton onClick={addQuestionHandler}>SAVE</SaveButton>
+        </Buttons>
       </Container>
-      <VoidBox></VoidBox>
     </>
   );
 };
