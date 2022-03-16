2d541534dc51ec3bea256f7dbf5e824a69ae69f8
Josep Conejero <bermeu@gmail.com>
2022-03-16 22:31:58 +0100

    Modified lf in postman file

--- src/components/MainQuestionBody/MainQuestionBody.test.js
+++ src/components/MainQuestionBody/MainQuestionBody.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from "@testing-library/react";
+/* import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import MainQuestionBody from "./MainQuestionBody";
+import MainQuestionBody from "./MainQuestionBody"; */
 
 describe("Given a MainQuestionBody component", () => {
   describe("When it's rendered with a list and screen='questions'", () => {
