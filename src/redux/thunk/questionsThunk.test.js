7667583aca4787f6e464a8d46169b9fcc7bcde1a
Josep Conejero <bermeu@gmail.com>
2022-03-17 19:49:46 +0100

    Added loadQuestionsList and updateQuestion to questionsThunk.test

--- src/redux/thunk/questionsThunk.test.js
+++ src/redux/thunk/questionsThunk.test.js
@@ -1,11 +1,26 @@
-/* import { loadQuestionsListThunk } from "./questionsThunk"; */
+import { loadQuestionsListThunk, loadOneQuestionThunk } from "./questionsThunk";
 
 describe("Given a loadQuestionsListThunk function", () => {
   describe("When it is called", () => {
     test("Then it should call the dispatch function", async () => {
-      /*  const dispatch = jest.fn();
+      const dispatch = jest.fn();
 
       await loadQuestionsListThunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a loadOneQuestionThunk function", () => {
+  describe("When it's called", () => {
+    test("Then it should dispatch a function", async () => {
+      /* const dispatch = jest.fn();
+      const idQuestion = "1";
+
+      const loadOneQuestion = loadOneQuestionThunk(idQuestion);
+
+      await loadOneQuestion(dispatch);
 
       expect(dispatch).toHaveBeenCalled(); */
     });
