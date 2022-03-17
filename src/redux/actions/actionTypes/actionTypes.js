1666f1285ada3b3495b1a4a7de1a93d3e244e1f2
Josep Conejero <bermeu@gmail.com>
2022-03-17 09:31:14 +0100

    Added loadOneQuestion to actionTypes

--- src/redux/actions/actionTypes/actionTypes.js
+++ src/redux/actions/actionTypes/actionTypes.js
@@ -1,5 +1,6 @@
 const actionTypes = {
   loadQuestions: "load-questions",
+  loadOneQuestion: "load-one-question",
   deleteQuestion: "delete-question",
   addQuestion: "add-question",
   updateQuestion: "update-question",
