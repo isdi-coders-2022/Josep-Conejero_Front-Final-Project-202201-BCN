5bbcaf0d2dddaa7da0f965f2f404bfb43a6e8227
Josep Conejero <bermeu@gmail.com>
2022-03-18 18:18:06 +0100

    Modified /reducers/index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,13 +3,13 @@ import { combineReducers } from "redux";
 import oneQuestionReducer from "./oneQuestionReducer";
 import questionsReducer from "./questionsReducer";
 import oneQuestionsListReducer from "./oneQuestionsListReducer";
-/*import questionsListsReducer from "./questionsListsReducer"; */
+import questionsListsReducer from "./questionsListsReducer";
 
 const rootReducer = combineReducers({
   oneQuestionData: oneQuestionReducer,
   questionsData: questionsReducer,
   oneQuestionsListData: oneQuestionsListReducer,
-  /* questionsListsData: questionsListsReducer, */
+  questionsListsData: questionsListsReducer,
 });
 
 export default rootReducer;
