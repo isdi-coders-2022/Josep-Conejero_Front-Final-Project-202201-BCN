41465557647890574c3540b67a3b832048fb2879
Josep Conejero <bermeu@gmail.com>
2022-03-21 19:46:52 +0100

    Removed lf from userThunk

--- src/redux/thunk/userThunk.js
+++ src/redux/thunk/userThunk.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import {
   createUserAction,
-  /* loadProfileAction, */
   loginUserAction,
 } from "../actions/actionsCreator/actionsCreator";
 import toast from "react-hot-toast";
@@ -26,9 +25,6 @@ export const loginThunk = (user, navigate) => async (dispatch) => {
       navigate("/home");
     })
     .catch((error) => {
-      /* debugger; */
-      /*  dispatch(errorOnRegisterAction(error.response.data));
-      toast.error(`${error.response.data.message}`, {*/
       toast.error(`USER OR PASSWORD ARE WRONG`, {
         duration: 2000,
         style: {
@@ -46,17 +42,6 @@ export const loginThunk = (user, navigate) => async (dispatch) => {
 };
 
 export const createUserThunk = (user, navigate) => async (dispatch) => {
-  /* const data = new FormData(); */
-  /* data.append("image", user.image);*/
-  /* data.append("name", user.name);
-  data.append("username", user.username);
-  data.append("password", user.password); */
-  /* const data = {
-    name: user.name,
-    username: user.username,
-    password: user.password,
-  }; */
-
   const data = JSON.stringify({
     name: user.name,
     username: user.username,
@@ -73,8 +58,6 @@ export const createUserThunk = (user, navigate) => async (dispatch) => {
     body: JSON.stringify(user),
   };
 
-  /*  const config = { headers: { "content-type": "multipart/form-data" } };
-   */
   axios
     .post(url, data, config)
     .then(() => {
@@ -82,9 +65,6 @@ export const createUserThunk = (user, navigate) => async (dispatch) => {
       navigate("/login");
     })
     .catch((error) => {
-      /* debugger; */
-      /*  dispatch(errorOnRegisterAction(error.response.data));
-      toast.error(`${error.response.data.message}`, {*/
       toast.error(`USER EXISTS`, {
         duration: 2000,
         style: {
@@ -100,59 +80,3 @@ export const createUserThunk = (user, navigate) => async (dispatch) => {
       });
     });
 };
-
-/* export const loadProfileThunk = (token) => async (dispatch) => {
-  const url = `${process.env.REACT_APP_API_URL_USER}/user`;
-
-  const config = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
-  debugger;
-
-  axios.get(url, config).then((response) => {
-    dispatch(loadProfileAction(response.data.actualUser));
-  });
-}; */
-
-/* import { axios } from "axios";
-import jwtDecode from "jwt-decode";
-import {
-  errorOnLoginAction,
-  errorOnRegisterAction,
-  loginAction,
-} from "../actions/actionsCreator/actionsCreator";
-
-export const registerThunk = (userData) => async (dispatch) => {
-  const url = `${process.env.REACT_APP_API_URL}/user/register`;
-
-  await axios
-    .post(url, userData, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    .then(() => {})
-    .catch((error) => {
-      dispatch(errorOnRegisterAction(error.response.data));
-    });
-};
-
-export const loginThunk = (userData) => async (dispatch) => {
-  const url = `${process.env.REACT_APP_API_URL}/user/login`;
-
-  await axios
-    .post(url, userData)
-    .then((response) => {
-      localStorage.setItem("tokenKey", response.data.token);
-      const userInfo = jwtDecode(response.data.token);
-      dispatch(loginAction(userInfo));
-    })
-    .catch((error) => {
-      dispatch(errorOnLoginAction(error.response.data));
-    });
-};
- */
