1105562d477f4ce987eb53d200a69b9522363db4
Josep Conejero <bermeu@gmail.com>
2022-03-23 19:14:50 +0100

    Added Página en construcción to QuestionsListPage

--- src/components/pages/QuestionsListPage.js
+++ src/components/pages/QuestionsListPage.js
@@ -2,7 +2,6 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import MainBody from "../MainBody/MainBody";
 import {
-  /* textIconQuestionsList, */
   iconAllQuestionsPath,
   backgroundColorIconAllQuestions,
   alternativeTextIconAllQuestions,
@@ -12,13 +11,11 @@ export const QuestionsListPage = () => {
   return (
     <>
       <Header
-        /*  title={textIconQuestionsList} */
-        title={"ESTA ES UNA DE ESAS LISTAS"}
+        title={"Página en construcción"}
         picture={iconAllQuestionsPath}
         backgroundColor={backgroundColorIconAllQuestions}
         alternativeTextImage={alternativeTextIconAllQuestions}
       ></Header>
-      {/* <p>ESTA ES LA LISTA DE PREGUNTAS DE LA LISTA EN LA QUE HEMOS PULSADO</p> */}
       <MainBody></MainBody>
       <Footer></Footer>
     </>
